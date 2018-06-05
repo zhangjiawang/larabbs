09541fe9660d0e0fa04e26c27e8de777a90e2648
zhangjiawang <zhangjiawang99@gmail.com>
2018-06-05 01:56:37 +0000

    Basic user profile editing

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,6 +24,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        \Carbon\Carbon::setLocale('zh');
 
         //
     }
