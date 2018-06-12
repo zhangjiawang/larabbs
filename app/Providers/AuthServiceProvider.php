b6de90cb01051a2a197e27d349ed48abea72a907
zhangjiawang <zhangjiawang99@gmail.com>
2018-06-12 01:56:50 +0000

    Seeding topics and users data

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -11,6 +11,7 @@ class AuthServiceProvider extends ServiceProvider {
 	 * @var array
 	 */
 	protected $policies = [
+		 \App\Models\Topic::class => \App\Policies\TopicPolicy::class,
 		'App\Model' => 'App\Policies\ModelPolicy',
 		\App\Models\User::class => \App\Policies\UserPolicy::class,
 	];
--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,5 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 //User Routes...
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
