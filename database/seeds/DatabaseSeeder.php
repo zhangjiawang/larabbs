d7eadffeabdde9f060e43fd18c384184313498ca
zhangjiawang <zhangjiawang99@gmail.com>
2018-06-13 06:25:50 +0000

    User replies list

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,14 +2,17 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder {
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run() {
-		$this->call(UsersTableSeeder::class);
-		$this->call(TopicsTableSeeder::class);
-	}
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(UsersTableSeeder::class);
+        $this->call(TopicsTableSeeder::class);
+        $this->call(ReplysTableSeeder::class);
+    }
 }
